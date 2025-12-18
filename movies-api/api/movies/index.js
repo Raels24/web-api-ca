ef4eb79d10c88210ebe9d083f4d84c87120d2b2b
sante <santersraels0@gmail.com>
2025-12-18 22:34:08 +0000

    add backend movie endpoints from TMDB

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,15 +1,38 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { getMovies } from '../tmdb-api'; 
+import { getMovies, getMovie, getPopularMovies, getTopRatedMovies, getTrendingMovies, getUpcomingMovies } from '../tmdb-api'; 
 
 
 const router = express.Router();
 
-// movie routes to be added
 router.get('/discover', asyncHandler(async (req, res) => {
     const discoverMovies = await getMovies();
     res.status(200).json(discoverMovies);
 }));
 
+router.get('/upcoming', asyncHandler(async (req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+}));
+
+router.get('/popular', asyncHandler(async (req, res) => {
+    const popularMovies = await getPopularMovies();
+    res.status(200).json(popularMovies);
+}));
+
+router.get('/toprated', asyncHandler(async (req, res) => {
+    const topRatedMovies = await getTopRatedMovies();
+    res.status(200).json(topRatedMovies);
+}));
+
+router.get('/trending', asyncHandler(async (req, res) => {
+    const trendingMovies = await getTrendingMovies();
+    res.status(200).json(trendingMovies);
+}));
+
+router.get('/:id', asyncHandler(async (req, res) => {
+    const movie = await getMovie(req.params.id);
+    res.status(200).json(movie);
+}));
 
 export default router;
--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -1,13 +1,37 @@
 import fetch from 'node-fetch';
 
-export const getMovies = async () => {
-    const response = await fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
-    );
+const TMDB_BASE = 'https://api.themoviedb.org/3';
 
+const tmdbFetch = async (path) => {
+    const response = await fetch(`${TMDB_BASE}${path}`);
     if (!response.ok) {
         throw new Error(response.json().message);
     }
-
     return await response.json();
 };
+
+export const getMovies = async () => {
+    return await tmdbFetch(`/discover/movie?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`);
+};
+    
+export const getMovie = async (id) => {
+    return await tmdbFetch(`/movie/${id}?api_key=${process.env.TMDB_KEY}&language=en-US`);
+};
+
+export const getUpcomingMovies = async () => {
+    return await tmdbFetch(`/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`);
+};
+
+export const getPopularMovies = async () => {
+    return await tmdbFetch(`/movie/popular?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`);
+};
+
+export const getTopRatedMovies = async () => {
+    return await tmdbFetch(`/movie/top_rated?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`);
+};
+
+export const getTrendingMovies = async () => {
+    return await tmdbFetch(`/trending/movie/week?api_key=${process.env.TMDB_KEY}`);
+};
+
+
